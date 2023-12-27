38a7d307b142ffdb3476885489233cade965150e
paulatalim <paulactalim@gmail.com>
2023-12-27 15:18:08 -0300

    retirada de codigo lixo

--- sqlite/lib/main.dart
+++ sqlite/lib/main.dart
@@ -1,7 +1,5 @@
 import './armazenamento.dart';
 import 'package:flutter/material.dart';
-// import 'package:sqflite/sqflite.dart';
-// import 'package:path/path.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
@@ -14,97 +12,6 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Armazenamento storage = Armazenamento();
-
-  // _recuperarBancoDados() async {
-  //   final caminhoBancoDados = await getDatabasesPath();
-  //   final localBancoDados = join(caminhoBancoDados, "banco3.bd");
-  //   var bd = await openDatabase(localBancoDados, version: 1,
-  //       onCreate: (db, dbVersaoRecente) {
-  //     String sql =
-  //         "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER) ";
-  //     db.execute(sql);
-  //   });
-  //   return bd;
-  //   //print("aberto: " + bd.isOpen.toString() );
-  // }
-
-  // _salvarDados(String nome, int idade) async {
-  //   Database bd = await _recuperarBancoDados();
-  //   Map<String, dynamic> dadosUsuario = {"nome": nome, "idade": idade};
-  //   int id = await bd.insert("usuarios", dadosUsuario);
-  //   print("Salvo: $id ");
-  // }
-
-  // _listarUsuarios() async {
-  //   Database bd = await _recuperarBancoDados();
-  //   String sql = "SELECT * FROM usuarios";
-  //   //String sql = "SELECT * FROM usuarios WHERE idade=58";
-  //   //String sql = "SELECT * FROM usuarios WHERE idade >=30 AND idade <=58";
-  //   //String sql = "SELECT * FROM usuarios WHERE idade BETWEEN 18 AND 58";
-  //   //String sql = "SELECT * FROM usuarios WHERE nome='Maria Silva'";
-  //   List usuarios =
-  //       await bd.rawQuery(sql); //conseguimos escrever a query que quisermos
-  //   for (var usu in usuarios) {
-  //     print(" id: " +
-  //         usu['id'].toString() +
-  //         " nome: " +
-  //         usu['nome'] +
-  //         " idade: " +
-  //         usu['idade'].toString());
-  //   }
-  // }
-
-  // _listarUmUsuario(int id) async {
-  //   Database bd = await _recuperarBancoDados();
-  //   List usuarios = await bd.query("usuarios",
-  //       columns: ["id", "nome", "idade"], where: "id = ?", whereArgs: [id]);
-  //   for (var usu in usuarios) {
-  //     print(" id: " +
-  //         usu['id'].toString() +
-  //         " nome: " +
-  //         usu['nome'] +
-  //         " idade: " +
-  //         usu['idade'].toString());
-  //   }
-  // }
-
-  // /*_excluirUsuario(int id) async{
-  //   Database bd = await _recuperarBancoDados();
-  //   int retorno = await bd.delete(
-  //       "usuarios",
-  //       where: "id = ?",  //caracter curinga
-  //       whereArgs: [id]
-  //   );
-  //   print("Itens excluidos: "+retorno.toString());
-  // }*/
-
-  // _excluirUsuario(int id) async {
-  //   Database bd = await _recuperarBancoDados();
-  //   int retorno = await bd.delete("usuarios",
-  //       // where: "nome = ? AND idade = ?",  //caracter curinga
-  //       // whereArgs: ["Raquel Ribeiro", 26]
-  //       where: "id = ?", //caracter curinga
-  //       whereArgs: [id]);
-  //   print("Itens excluidos: " + retorno.toString());
-  // }
-
-  // _atualizarUsuario(int id) async {
-  //   Database bd = await _recuperarBancoDados();
-  //   Map<String, dynamic> dadosUsuario = {
-  //     "nome": "Antonio Pedro",
-  //     "idade": 35,
-  //   };
-  //   int retorno = await bd.update("usuarios", dadosUsuario,
-  //       where: "id = ?", //caracter curinga
-  //       whereArgs: [id]);
-  //   print("Itens atualizados: " + retorno.toString());
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   storage = Armazenamento();
-  // }
 
   @override
   Widget build(BuildContext context) {
