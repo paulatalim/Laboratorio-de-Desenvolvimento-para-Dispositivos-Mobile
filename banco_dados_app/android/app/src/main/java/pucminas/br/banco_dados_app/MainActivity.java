590651b6defc2eb24f40239e4f7b80e665ba120f
paulatalim <paulactalim@gmail.com>
2023-12-27 15:33:48 -0300

    delete project

--- banco_dados_app/android/app/src/main/java/pucminas/br/banco_dados_app/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package pucminas.br.banco_dados_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- banco_dados_app/android/app/src/main/kotlin/pucminas/br/banco_dados_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package pucminas.br.banco_dados_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
